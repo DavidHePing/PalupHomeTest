06f2ff09dc20bea47c965dca93b714eaf8e91a00
Dav He <s991179@gmail.com>
2025-03-22 13:10:01 +0800

    feat: add authorization middlewarre for api key

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ const APIKey = "qwerklj1230dsa350123l2k1j4kl1j24"
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
-
+	router.Use(apiKeyAuthMiddleware)
 	router.HandleFunc("/test-1", Test1).Methods("POST")
 
 	port := 8082
@@ -21,6 +21,17 @@ func handleRequests() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func apiKeyAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("api-key")
+		if apiKey != APIKey {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
